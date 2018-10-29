33083832e12498ff2de8728f20850e46a9766d90
Ryan Butler <rbutler@digitalocean.com>
2018-10-29 16:09:25 -0500

    user landing page

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -168,6 +168,7 @@ instance Yesod App where
     isAuthorized RobotsR _ = return Authorized
     isAuthorized (RunR _) _ = return Authorized
     isAuthorized AllRunsR _ = return Authorized
+    isAuthorized (UserR _) _ = return Authorized
     isAuthorized (StaticR _) _ = return Authorized
 
     -- the profile route requires that the user is authenticated, so we
