f953f6cd1aa2fdd966307f134f865ffee997752f
Ryan Butler <butler.ryan@gmail.com>
2019-09-12 10:12:44 -0500

    beginning to group messages

--- src/Handler/Run.hs
+++ src/Handler/Run.hs
@@ -14,6 +14,8 @@ import Network.HTTP.Simple
 import Text.Printf
 import Data.Aeson
 import System.Environment
+import qualified Prelude as P
+import qualified Data.Map.Strict as Map
 
 getRunR :: RunId -> Handler Html
 getRunR runId = do
@@ -33,7 +35,8 @@ getAllRunsR = do
 
 getUpdateRunsR :: Handler Value
 getUpdateRunsR = do
-  messages <- getMessagesFromGroupme
+  messages <- getMessagesFromGroupme []
+  let stats = generateStats messages
   returnJson $ messages
   --let stats = statsFromMessages
   --createRun
@@ -64,7 +67,7 @@ data Message = Message
   , userID :: String
   , name :: String
   , groupID :: String
-  , text :: String
+  --, text :: Maybe String
   , favoritedBy :: [String]
   } deriving (Generic, Show)
 
@@ -74,21 +77,97 @@ instance ToJSON Message
 
 customOptions = defaultOptions { fieldLabelModifier = camelTo2 '_' }
 
---instance ToJSON Message where
-  --toJSON m = object
-    --[ "name" .= name Message ]
-getMessagesFromGroupme :: Handler MessageResponse
-getMessagesFromGroupme = do
+{-
+getMessagesFromGroupme :: [Message] -> Handler [Message]
+getMessagesFromGroupme acc = do
   App {..} <- getYesod
-  let limit = 1
-      endpoint = "GET https://api.groupme.com/v3/groups/" ++ groupmeGroupID ++ "/messages?token=" ++ groupmeToken ++ "&limit=" ++ show limit 
-      -- ++ "&before_id=" ++ show 0
+  let limit = 100
+      endpoint = "GET https://api.groupme.com/v3/groups/" ++ groupmeGroupID ++ "/messages?token=" ++ groupmeToken ++ "&limit=" ++ show limit ++ beforeParams
+  case acc of
+    [] -> print $ "first -- " ++ (show $ length acc)
+    x -> print $ (Handler.Run.id $ P.last x) ++ " -- " ++ (show $ length acc)
+  request <- parseRequest endpoint
+  resp <- httpJSON $ request
+  let responseCode = getResponseStatusCode resp
+  print responseCode
+  case responseCode of
+    304 -> return acc
+    _ -> do
+           let body = getResponseBody resp :: MessageResponse
+               m = messages $ response body
+           getMessagesFromGroupme (acc ++ m)
+  where beforeParams = case acc of
+                         [] -> ""
+                         x -> "&before_id=" ++ (Handler.Run.id $ P.last x)
+-}
+
+getMessagesFromGroupme :: [Message] -> Handler [Message]
+getMessagesFromGroupme acc = do
+  App {..} <- getYesod
+  let limit = 100
+      endpoint = "GET https://api.groupme.com/v3/groups/" ++ groupmeGroupID ++ "/messages?token=" ++ groupmeToken ++ "&limit=" ++ show limit ++ beforeParams
+  case acc of
+    [] -> print $ "first -- " ++ (show $ length acc)
+    x -> print $ (Handler.Run.id $ P.last x) ++ " -- " ++ (show $ length acc)
   request <- parseRequest endpoint
   resp <- httpJSON $ request
   let body = getResponseBody resp :: MessageResponse
-  print body
-  return body
+      m = messages $ response body
+  if (length m) < limit
+  then return (acc ++ m)
+  else getMessagesFromGroupme (acc ++ m)
+  where beforeParams = case acc of
+                         [] -> ""
+                         x -> "&before_id=" ++ (Handler.Run.id $ P.last x)
 
+--instance ToJSON Message where
+  --toJSON m = object
+    --[ "name" .= name Message ]
+{-
+getMessagesFromGroupme :: Handler MessageResponse
+getMessagesFromGroupme = do
+  return $ getLimitedMessages []
+
+getLimitedMessages acc = do
+  App {..} <- getYesod
+  let limit = 100
+      endpoint = "GET https://api.groupme.com/v3/groups/" ++ groupmeGroupID ++ "/messages?token=" ++ groupmeToken ++ "&limit=" ++ show limit ++ beforeParams
+  request <- parseRequest endpoint
+  resp <- httpJSON $ request
+  let body = getResponseBody resp :: MessageResponse
+      m = messages $ response body
+  return $ case m of
+             [] -> m
+             x -> x ++ (getLimitedMessages x)
+  where beforeParams = case acc of
+                         [] -> ""
+                         x -> "&before_id=" ++ (Handler.Run.id $ last x)
+-}
+
+data Stat = Stat
+  { sUserID :: String
+  , sUserName :: String
+  , messages :: Int
+  , heartsReceived :: Int
+  , heartsGiven :: Int
+
+updateValue :: Maybe Stat -> Message -> Maybe Stat
+updateValue
+  | Nothing m = Just Stat
+  | (Just s) m = Just Stat
+                    { sUserID = sUserID s
+                    , sUserName = sUserName 
+                    , messages = (messages s) + 1
+                    , heartsReceived = (heartsReceived s) + (length $ favoritedBy)
+                    }
+  
+
+groupedMessages messages =
+  let m - Map.empty :: Map string Stat
+  
+
+generateStats messages = do
+  let g = groupedMessages messages
   
   
 
